d707d7492e4bef4a8b45dd94c756c0afa9d479aa
Beyza Butun <37122991+beyzabutun@users.noreply.github.com>
2019-05-07 18:34:32 +0300

    Update reorganize.py

--- Source/reorganize.py
+++ Source/reorganize.py
@@ -2,10 +2,10 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 import os
-from shutil import copyfile
+from shutil import move
 import seaborn as sns
 
-train_data = pd.read_csv('../Doc/train.csv')
+train_data = pd.read_csv('../input/train.csv')
 
 target_dir = "../Data/reorganized"
 root_dir = "../Data/resized"
@@ -15,8 +15,9 @@ for dirName, _, fileList in os.walk(root_dir):
 		imageID = file[:-4]
 		train_data[train_data.id == imageID]
 		landmark_id = str(train_data[train_data.id == imageID].landmark_id.get_values()[0])
-		landmark_dir = target_dir + "/" + landmark_id
-		copyfile(dirName + "/" + file, landmark_dir)
-
+		landmark_dir = target_dir + "/" + landmark_id 
+		if not os.path.exists(landmark_dir):
+			os.mkdir(landmark_dir)
+		move(dirName + "/" + file, landmark_dir + "/" + imageID + ".jpg")
 
 

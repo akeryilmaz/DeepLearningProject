369e9f0c970d8c0ba4229aec96178b4a053f04f0
Buğra Aker Yılmaz <akerylm@gmail.com>
2019-05-08 12:42:59 +0300

    Updated reorganize for Ubuntu

--- Source/reorganize.py
+++ Source/reorganize.py
@@ -1,12 +1,13 @@
+
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
 from shutil import move
 
-top10_data = pd.read_csv('C:\\Users\\User\\Desktop\\Source\\top10.csv')
+top10_data = pd.read_csv('../Doc/top10.csv')
 
-target_dir = "C:\\Users\\User\\Desktop\\top10"
-root_dir = "C:\\Users\\User\\Desktop\\DLResized"
+target_dir = "../Data/top10"
+root_dir = "../Data/resized"
 top10 = [138982, 62798, 177870, 176528, 192931, 126637, 83144, 171772, 20409, 151942]
 
 i=0
@@ -20,13 +21,13 @@ for dirName, _, fileList in os.walk(root_dir):
         if len(landmark_id) == 0:
             continue
         landmark_id = landmark_id[0]
-        landmark_dir = target_dir + "\\" + str(landmark_id)
+        landmark_dir = target_dir + "/" + str(landmark_id)
         if not os.path.exists(landmark_dir):
             print("created")
-            os.mkdir(landmark_dir)
+            os.mkdir(os.path.abspath(landmark_dir))
         c+=1
         print('counter: ' ,c )
-        move(dirName + "\\" + file, landmark_dir + "\\" + imageID + ".jpg")
+        move(dirName + "/" + file, landmark_dir + "/" + imageID + ".jpg")
         '''landmark_id = train_data[train_data.id == imageID].landmark_id.get_values()[0]
         if landmark_id not in top10:
             continue

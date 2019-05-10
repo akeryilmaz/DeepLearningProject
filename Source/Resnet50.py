c6a3056937d4cc972f0bb200c8ea9f8910bb2ca4
Buğra Aker Yılmaz <akerylm@gmail.com>
2019-05-10 18:42:40 +0300

    Minor change in Resnet50.py
    Experiment settings have been replaced to the beginning of the file

--- Source/Resnet50.py
+++ Source/Resnet50.py
@@ -12,6 +12,25 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data.sampler import SubsetRandomSampler
 
+BATCHSIZE = 64
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+																		std=[0.229, 0.224, 0.225])
+TRAINTRANSFORMS = transforms.Compose([transforms.Resize(224),
+																		transforms.ToTensor(),
+																		normalize,
+																		])
+TESTTRANSFORMS = transforms.Compose([transforms.Resize(224),
+																			transforms.ToTensor(),
+																			normalize,
+																			])
+PRETRAINED = True
+LOSS = nn.CrossEntropyLoss()
+OPTIMIZER = optim.Adam
+LEARNINGRATE = 0.01
+NUMEPOCHS = 10
+PRINT_FREQ = 100
+FILENAME = 'resnet50PretrainedBatch64.pth'
+
 #Set this to True if model is already trained
 skip_training = False
 
@@ -34,18 +53,10 @@ if skip_training:
 #TODO: Test below function
 
 def load_split_train_test(datadir, valid_size = .2):
-	normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-																		std=[0.229, 0.224, 0.225])
 
-	train_transforms = transforms.Compose([transforms.Resize(224),
-																		transforms.ToTensor(),
-																		normalize,
-																		])
+	train_transforms = TRAINTRANSFORMS
 
-	test_transforms = transforms.Compose([transforms.Resize(224),
-																			transforms.ToTensor(),
-																			normalize,
-																			])
+	test_transforms = TESTTRANSFORMS
 
 	train_data = torchvision.datasets.ImageFolder(datadir,
 									transform=train_transforms)
@@ -61,9 +72,9 @@ def load_split_train_test(datadir, valid_size = .2):
 	train_sampler = SubsetRandomSampler(train_idx)
 	test_sampler = SubsetRandomSampler(test_idx)
 	trainloader = torch.utils.data.DataLoader(train_data,
-									sampler=train_sampler, batch_size=32)
+									sampler=train_sampler, batch_size=BATCHSIZE)
 	testloader = torch.utils.data.DataLoader(test_data,
-									sampler=test_sampler, batch_size=32)
+									sampler=test_sampler, batch_size=BATCHSIZE)
 	return trainloader, testloader
 
 
@@ -83,21 +94,21 @@ def compute_accuracy(net, testloader):
 
 trainloader, testloader = load_split_train_test(data_dir)
 
-net = torchvision.models.resnet18(pretrained=True)
+net = torchvision.models.resnet18(pretrained=PRETRAINED)
 net.fc = nn.Linear(net.fc.in_features, 47)
 net.to(device)
 
 #Training settings
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters(), lr=0.01)
-n_epochs = 10
+criterion = LOSS
+optimizer = OPTIMIZER(net.parameters(), lr=LEARNINGRATE)
+n_epochs = NUMEPOCHS
 
 print("Starting Training.")
 #Training Loop
-net.train()
 for epoch in range(n_epochs):
+	net.train()
 	running_loss = 0.0
-	print_every = 200  # mini-batches
+	print_every = PRINT_FREQ  # mini-batches
 	for i, (inputs, labels) in enumerate(trainloader, 0):
 		# Transfer to GPU
 		inputs, labels = inputs.to(device), labels.to(device)
@@ -129,7 +140,7 @@ for epoch in range(n_epochs):
 print('Finished Training')
 
 # Save the network to a file
-filename = 'resnet50Pretrained.pth'
+filename = FILENAME
 if not skip_training:
 	try:
 		do_save = input('Do you want to save the model (type yes to confirm)? ').lower()

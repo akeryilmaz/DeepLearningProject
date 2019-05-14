2a0f318f1f0779237ba8ab05954237e785d01cd5
Beyza Butun <37122991+beyzabutun@users.noreply.github.com>
2019-05-14 15:03:40 +0300

    Update NonLandmarkClassifier.py

--- Source/NonLandmarkClassifier.py
+++ Source/NonLandmarkClassifier.py
@@ -4,35 +4,21 @@ import pandas as pd
 from PIL import Image
 from cv2 import resize
 import matplotlib.pyplot as plt
-from vgg16_hybrid_places_1365 import *
+from shutil import move
+from vgg16_places_365 import *
 #print(os.listdir("../Data/threshold"))
 
-'''
-# VGG 16 Places 365 scripts in custom dataset
-os.chdir("/kaggle/input/keras-vgg16-places365/")
-from vgg16_places_365 import VGG16_Places365
-os.chdir("/kaggle/working/")
-'''
-
 # Get List of Images
-data_dir = '../Data/threshold/'
+data_dir = '../Data/splitData/train'
 class_information = pd.read_csv('../Doc/class_info_keras_classifier.csv')
-
-'''
-# Resize all images
-all_images_resized = []
-for filename in all_images:    
-    im = np.array(Image.open(image_samples + filename).resize((224, 224), Image.LANCZOS))    
-    all_images_resized.append(im)
-'''
-# Placeholders for predictions
-p0, p1, p2 = [], [], []
-
+nonlandmark_dir = '../Data/splitData/nonlandmarkTrain'
 # Places365 Model
-model = VGG16_Hybrid_1365(weights='places')
+model = VGG16_Places365(weights='places')
+data = pd.read_csv('../Doc/iflandmark.csv')
 
 # Loop through all images
 for dirName, _, files in os.walk(data_dir):
+	print (dirName)
 	for filename in files:
 		image_array = np.array(Image.open(dirName + "/" + filename))
 		# Predict Top N Image Classes
@@ -41,59 +27,27 @@ for dirName, _, files in os.walk(data_dir):
 		classes = np.argsort(model.predict(image_0)[0])
 		classes = classes.tolist()
 		probs = probs.tolist()
-		cumProb = 0.0
-		topClasses = []
-		while cumProb < 0.95:
-			topClasses.append(classes.pop())
-			cumProb += probs.pop()
+		nonlandmarkProb = 0.0
+		landmarkProb = 0.0
+		while landmarkProb < 0.15 :
+			currentClass = classes.pop()
+			iflandmark = data[data.index == currentClass].iflandmark.get_values()[0]
+			if iflandmark == 1:
+				nonlandmarkProb += probs.pop()
+			else:
+				landmarkProb += probs.pop()
+			if nonlandmarkProb >= 0.85:
+				#TODO copy the file to somewhere else
+				landmark_id = dirName.split("/")[-1]
+				class_path = nonlandmark_dir + "/" + landmark_id
+				if not os.path.exists(class_path):
+					os.mkdir(class_path)
+				move(dirName + "/" + filename,  class_path + "/" + filename)			
+				break
+		#print(cumProb, "\n")
+		#print(topClasses, "\n")
+		
 
-
-'''
-  topn_preds = np.argsort(model.predict(image)[0])[::-1][0:topn]
-
-  p0.append(topn_preds[0])
-  p1.append(topn_preds[1])
-  p2.append(topn_preds[2])
-
-# Create dataframe for later usage
-topn_df = pd.DataFrame()
-topn_df['filename'] = np.array(all_images)
-topn_df['p0'] = np.array(p0)
-topn_df['p1'] = np.array(p1)
-topn_df['p2'] = np.array(p2)
-topn_df.to_csv('topn_class_numbers.csv', index = False)
-
-# Summary
-topn_df.head()
-
-
-# Get 'landmark' images
-n = 9
-landmark_images =  topn_df[topn_df['p0_landmark'] == 'landmark']['filename']
-landmark_indexes = landmark_images[:n].index.values
-
-# Plot image examples
-fig = plt.figure(figsize = (16, 16))
-for index, im in zip(range(1, n+1), [ all_images_resized[i] for i in landmark_indexes]):
-    fig.add_subplot(3, 3, index)
-    plt.title(filename)
-    plt.imshow(im)
-
-
-# Get 'non-landmark' images
-n = 9
-landmark_images =  topn_df[topn_df['p0_landmark'] == 'non-landmark']['filename']
-landmark_indexes = landmark_images[:n].index.values
-
-
-# Plot image examples
-fig = plt.figure(figsize = (16, 16))
-for index, im in zip(range(1, n+1), [ all_images_resized[i] for i in landmark_indexes]):
-    fig.add_subplot(3, 3, index)
-    plt.title(filename)
-    plt.imshow(im)
-
-'''
 
 
 

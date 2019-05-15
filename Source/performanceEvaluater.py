6b30f702639a37c00aada34fcd2ffe76b46c51f9
Buğra Aker Yılmaz <akerylm@gmail.com>
2019-05-15 18:14:03 +0300

    GAP evaluater added

--- Source/performanceEvaluater.py
+++ Source/performanceEvaluater.py
@@ -47,14 +47,47 @@ def compute_accuracy(net, testloader):
 	net.eval()
 	correct = 0
 	total = 0
+	confidences = []
+	predictions = []
+	true_labels = [] 
 	with torch.no_grad():
 		for images, labels in testloader:
 			images, labels = images.to(device), labels.to(device)
 			outputs = net(images)
-			_, predicted = torch.max(outputs.data, 1)
+			confidence, predicted = torch.max(outputs.data, 1)
 			total += labels.size(0)
 			correct += (predicted == labels).sum().item()
-	return correct / total
+			confidences.append(confidence)
+			predictions.append(predictions)
+			true_labels.append(labels)
+	return correct / total, np.array(confidences), np.array(predictions), np.array(true_labels)
+
+def GAP_vector(pred, conf, true, return_x=False):
+    '''
+    Compute Global Average Precision (aka micro AP), the metric for the
+    Google Landmark Recognition competition. 
+    This function takes predictions, labels and confidence scores as vectors.
+    In both predictions and ground-truth, use None/np.nan for "no label".
+
+    Args:
+        pred: vector of integer-coded predictions
+        conf: vector of probability or confidence scores for pred
+        true: vector of integer-coded labels for ground truth
+        return_x: also return the data frame used in the calculation
+
+    Returns:
+        GAP score
+    '''
+    x = pd.DataFrame({'pred': pred, 'conf': conf, 'true': true})
+    x.sort_values('conf', ascending=False, inplace=True, na_position='last')
+    x['correct'] = (x.true == x.pred).astype(int)
+    x['prec_k'] = x.correct.cumsum() / (np.arange(len(x)) + 1)
+    x['term'] = x.prec_k * x.correct
+    gap = x.term.sum() / x.true.count()
+    if return_x:
+        return gap, x
+    else:
+        return gap
 
 testloader = load_test(data_dir)
 
@@ -67,8 +100,14 @@ net.load_state_dict(torch.load(filename, map_location=lambda storage, loc: stora
 net.to(device)
 print('Model loaded from %s \n' % filename)
 
-accuracy = compute_accuracy(net, testloader)
+accuracy, conf, pred, true = compute_accuracy(net, testloader)
 print('Accuracy of the network on the test images: %.3f' % accuracy)
+
+gap, x = GAP_vector(pred, conf, true, return_x = True)
+
+print('GAP of the network on the test images: %.3f' % gap)
+
+print(x)
 
 
 

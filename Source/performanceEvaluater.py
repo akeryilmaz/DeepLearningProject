2c2ad7953191a3e845b9ee9ea841969499246447
Beyza Butun <37122991+beyzabutun@users.noreply.github.com>
2019-05-16 14:54:06 +0300

    Update performanceEvaluater.py

--- Source/performanceEvaluater.py
+++ Source/performanceEvaluater.py
@@ -1,47 +1,11 @@
-import os
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-import torch
-import torchvision
-import torchvision.transforms as transforms
-from torch.optim.lr_scheduler import MultiStepLR
-
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.utils.data.sampler import SubsetRandomSampler
-
-BATCHSIZE = 256
-
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-																		std=[0.229, 0.224, 0.225])
-TESTTRANSFORMS = transforms.Compose([transforms.Resize(224),
-																			transforms.ToTensor(),
-																			normalize,
-																			])
-
-MODEL = torchvision.models.densenet121
-FILENAME = 'densenet121SplitData'
-
-
-device = torch.device('cuda:0')
-#device = torch.device('cpu')
-
-if os.path.isdir('../Data/splitDataNonlandmark/test'):
-	data_dir = '../Data/splitDataNonlandmark/test'
-else:
-	#TODO Change the error given here.
-	raise Exception("Data directry not found!")
 
 def load_test(datadir):
-	test_transforms = TESTTRANSFORMS
-	test_data = torchvision.datasets.ImageFolder(datadir,
-									transform=test_transforms)
-	testloader = torch.utils.data.DataLoader(test_data,
-									shuffle=True, batch_size=BATCHSIZE)
-	return testloader
+        test_transforms = TESTTRANSFORMS
+        test_data = torchvision.datasets.ImageFolder(datadir,
+								        transform=test_transforms)
+        testloader = torch.utils.data.DataLoader(test_data,
+								        shuffle=True, batch_size=BATCHSIZE)
+        return testloader
 
 def compute_accuracy_and_GAP(net, testloader, return_result):
 	net.eval()
@@ -50,7 +14,6 @@ def compute_accuracy_and_GAP(net, testloader, return_result):
 	result = pd.DataFrame(columns = ["pred", "conf", "true"])
 	with torch.no_grad():
 		for i, (images, labels) in enumerate(testloader):
-			print (i)
 			images, labels = images.to(device), labels.to(device)
 			outputs = net(images)
 			confidence, predicted = torch.max(outputs.data, 1)
@@ -67,22 +30,3 @@ def compute_accuracy_and_GAP(net, testloader, return_result):
 		return correct / total, gap, result
 	else:
 		return correct / total, gap
-
-testloader = load_test(data_dir)
-
-filename = "../Doc/Models/"+ FILENAME + ".pth"
-
-net = MODEL()
-net.classifier = nn.Linear(net.classifier.in_features, 47)
-net.load_state_dict(torch.load(filename, map_location=lambda storage, loc: storage))
-net.to(device)
-print('Model loaded from %s \n' % filename)
-
-accuracy, gap, result = compute_accuracy_and_GAP(net, testloader, return_result = True)
-print('Accuracy of the network on the test images: %.3f' % accuracy)
-print('GAP of the network on the test images: %.3f' % gap)
-result.to_csv("../Doc/densenet121SplitDataTestResults.csv")
-
-
-
-
